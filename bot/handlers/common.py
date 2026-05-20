5fcd7cafc0445eb535982233d09f4cb5d838ecad
dudha369 <72561593+dudha369@users.noreply.github.com>
2026-05-20 12:42:34 +0300

    Update common.py

--- bot/handlers/common.py
+++ bot/handlers/common.py
@@ -2,8 +2,10 @@ from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
-from ..keyboards import main_markup
+import asyncio
+import asyncpg
 
+from ..keyboards import main_markup
 from db import User
 
 router = Router(name="common")
@@ -11,17 +13,24 @@ router = Router(name="common")
 
 @router.message(CommandStart())
 async def cmd_start(message: Message) -> None:
+    await message.answer(
+        text="Open the mini-app!",
+        reply_markup=main_markup
+    )
+
+    asyncio.create_task(save_user(message))
+
+
+async def save_user(message: Message):
     try:
-        await User.get_or_create(
-            id=message.from_user.id,
-            defaults={
-                "name": message.from_user.first_name or "Unknown",
-            },
+        await asyncio.wait_for(
+            User.get_or_create(
+                id=message.from_user.id,
+                defaults={
+                    "name": message.from_user.first_name or "Unknown"
+                }
+            ),
+            timeout=2.0
         )
     except Exception as e:
-        print(f"DB error in /start: {e}")
-    finally:
-        await message.answer(
-            text="Open the mini-app!",
-            reply_markup=main_markup
-        )
+        print("DB ERROR:", e)
